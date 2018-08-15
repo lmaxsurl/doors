6f84f1fb93abc7568b5a64ceebfb216f923e3d60
lmaxsurl <maxim.logunov.1998@mail.ru>
2018-08-15 16:29:22 +0300

    added crashlytics

--- presentation/src/main/java/ru/mail1998/logunov/maxim/doors/app/App.java
+++ presentation/src/main/java/ru/mail1998/logunov/maxim/doors/app/App.java
@@ -2,8 +2,10 @@ package ru.mail1998.logunov.maxim.doors.app;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.squareup.leakcanary.LeakCanary;
 
+import io.fabric.sdk.android.Fabric;
 import ru.mail1998.logunov.maxim.doors.injection.AppComponent;
 import ru.mail1998.logunov.maxim.doors.injection.AppModule;
 import ru.mail1998.logunov.maxim.doors.injection.DaggerAppComponent;
@@ -19,6 +21,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         appComponent = DaggerAppComponent
                 .builder()
                 .build();
