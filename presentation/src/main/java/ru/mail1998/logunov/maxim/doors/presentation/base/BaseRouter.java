950b84965eea02341416d5f07f0b5e9eb69abbfe
lmaxsurl <maxim.logunov.1998@mail.ru>
2018-08-15 23:39:41 +0300

    throw exceptions to Crashlytics

--- presentation/src/main/java/ru/mail1998/logunov/maxim/doors/presentation/base/BaseRouter.java
+++ presentation/src/main/java/ru/mail1998/logunov/maxim/doors/presentation/base/BaseRouter.java
@@ -2,6 +2,8 @@ package ru.mail1998.logunov.maxim.doors.presentation.base;
 
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
+
 import logunov.maxim.domain.entity.Error;
 import ru.mail1998.logunov.maxim.doors.R;
 
@@ -22,17 +24,16 @@ public abstract class BaseRouter<A extends BaseActivity> {
                 case SERVER_IS_NOT_AVAILABLE:
                     return activity.getResources().getString(R.string.error_server_not_available);
                 case SERVER_ERROR:
+                    Crashlytics.logException(throwable);
                     return activity.getResources().getString(R.string.error_server);
                 default: {
-                    Log.e(activity.getClass().getSimpleName(),
-                            "забыл указать тип ошибки " + throwable.toString());
+                    Crashlytics.logException(throwable);
                     return activity.getResources().getString(R.string.something_wrong);
                 }
             }
         } else {
-            Log.e(activity.getClass().getSimpleName(),
-                    "ужасная проблема " + throwable.toString());
-            return activity.getResources().getString(R.string.error);
+            Crashlytics.logException(throwable);
+            return activity.getResources().getString(R.string.something_wrong);
         }
     }
 }
